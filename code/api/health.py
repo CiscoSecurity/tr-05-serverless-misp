36dccca5797025c0e2cd10f34a92e5aaf7b608a8
AnastasiiaRozlyvan <48979187+AnastasiiaRozlyvan@users.noreply.github.com>
2021-04-09 17:20:39 +0300

    [CCTRI-1804] Implement health endpoint (#4)
    * [CCTRI-1804] Implement health endpoint

--- code/api/health.py
+++ code/api/health.py
@@ -1,10 +1,21 @@
-from api.utils import get_jwt, jsonify_data
-from flask import Blueprint
+from api.utils import get_key, jsonify_data
+from flask import Blueprint, current_app
+from pymisp import PyMISP, exceptions
+
+from api.errors import CriticalMISPResponseError
 
 health_api = Blueprint('health', __name__)
 
 
 @health_api.route('/health', methods=['POST'])
 def health():
-    _ = get_jwt()
+    try:
+        _ = PyMISP(
+            key=get_key(),
+            url=current_app.config['HOST'],
+            ssl=current_app.config['MISP_VERIFYCERT']
+        )
+    except exceptions.PyMISPError as error:
+        raise CriticalMISPResponseError(error.message)
+
     return jsonify_data({'status': 'ok'})
